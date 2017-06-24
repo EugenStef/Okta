b320a2b41d38b02a92bbd0026b655c4ea9ed3718
eugenstef <dumitru.eugen.gen@gmail.com>
2017-06-24 15:15:01 +0300

    -Changed the redirect URL to 'http://eugenstef.github.io/Okta-widget/loggedin.htm' - Changed the sign-out function redirect to 'index.html'

--- okta.js
+++ okta.js
@@ -3,7 +3,7 @@
 
 
     var orgUrl = 'https://atelier.oktapreview.com';
-var redirectUrl = 'http://eugenstef.github.io/loggedin.htm';
+var redirectUrl = 'http://eugenstef.github.io/Okta-widget/loggedin.htm';
 // var oktaSignIn = new OktaSignIn({baseUrl: orgUrl});
 var oktaSignIn = new OktaSignIn({
     baseUrl: orgUrl,
@@ -47,7 +47,7 @@ oktaSignIn.renderEl(
 
 function myFunction() {
    oktaSignIn.signOut(function () {
-window.location.href='/index.html';
+window.location.href='index.html';
 
     });
 }
