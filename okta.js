806a36a644d8489cb873d12548ab9f4dc2cfef8f
Dumitru Eugen <eu-gen@users.noreply.github.com>
2017-08-17 19:41:46 +0300

    Update okta.js

--- okta.js
+++ okta.js
@@ -62,7 +62,7 @@ oktaSignIn.renderEl(
       if (res.type === 'SESSION_STEP_UP' && res.stepUp) {
           console.log('Target resource url: ' + res.stepUp.url);
           res.stepUp.finish();
-	  res.session.setCookie;
+	  res.session.setCookiesetCookieAndRedirect();
 	      return;
       }
 	  else if (res.status === 'SUCCESS') {
