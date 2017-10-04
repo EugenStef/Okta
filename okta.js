bbbd35aed1323862f735a9379d41d8ce97f09ca9
Dumitru Eugen <eu-gen@users.noreply.github.com>
2017-10-05 00:33:15 +0300

    Update okta.js

--- okta.js
+++ okta.js
@@ -6,7 +6,7 @@
     }
 
 var orgUrl = 'https://mondiru.okta.com';
-var redirectUrl = 'https://mondiru-dev-ed.my.salesforce.com/';
+//var redirectUrl = 'https://mondiru-dev-ed.my.salesforce.com/';
 // var oktaSignIn = new OktaSignIn({baseUrl: orgUrl});
 var oktaSignIn = new OktaSignIn({
     baseUrl: orgUrl,
